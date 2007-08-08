75adaa85348e86a3cac5602610812e66fef1a81e
Henry Ludemann <henry@munchkin>
2007-08-08 11:23:25 -0700

    Add a download url to the python setup.py

--- setup.py
+++ setup.py
@@ -8,22 +8,25 @@ syntax, and have decoders automatically generated for you. Written in Python,
 it currently supports decoding to xml or python objects.
 """
 
+version='0.1.1'
+
 setup(name='bdec',
-      version='0.1.1',
+      version=version,
       description='bdec binary decoder',
       long_description=long_description,
       author='Henry Ludemann',
       author_email='misc@hl.id.au',
       url='http://www.hl.id.au/Projects/bdec/',
+      download_url='http://www.hl.id.au/Projects/bdec/files/bdec-0.1.1.tar.gz',
       packages=['bdec'],
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Environment :: Console',
           'Intended Audience :: Developers',
-          'License :: OSI Approved :: BSD License (revised)',
-          'Operating System :: OS Independant',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
           'Programming Language :: Python',
           'Topic :: Software Development :: Libraries :: Python Modules',
-          'Utilities',
+          'Topic :: Utilities',
           ],
      )
