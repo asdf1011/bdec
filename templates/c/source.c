0da8fd536a7bb1858945fb4272c0a14706546220
Henry Ludemann <henry@munchkin>
2007-10-07 18:19:06 -0700

    Restructure source decoding to remove redundancy

--- templates/c/source.c
+++ templates/c/source.c
@@ -1,28 +1,32 @@
 <%namespace file="/decodeentry.tmpl" name="decodeentry" />
 <%namespace file="/type.tmpl" name="ctype" />
-<%! from bdec.field import Field %>
+<%! 
+  from bdec.field import Field
+  from bdec.sequence import Sequence
+ %>
 
 #include <stdlib.h>
 
 #include "${entry.name}.h"
 #include "buffer.h"
 
-<%def name="decodeChildren(entry)">
+## Recursively create functions for decoding the entries contained within this protocol specification.
+<%def name="recursiveDecode(entry)">
 %for child in entry.children:
-%if not isinstance(child, Field):
-${child.name}* decode_${child.name}(Buffer* buffer)
-{
-    ${decodeentry.decode(child)}
-}
-
-${decodeChildren(child)}
-%endif
+  %if not isinstance(child, Field):
+${recursiveDecode(child)}
+  %endif
 %endfor
-</%def>
-
-${decodeChildren(entry)}
 
 ${entry.name}* decode_${entry.name}(Buffer* buffer)
 {
-    ${decodeentry.decode(entry)}
+  %if isinstance(entry, Field):
+    ${decodeField(entry, entry.name + " result")};
+    return result;
+  %elif isinstance(entry, Sequence):
+    ${decodeentry.decodeSequence(entry)}
+  %endif
 }
+</%def>
+
+${recursiveDecode(entry)}
