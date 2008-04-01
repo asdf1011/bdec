badb4cc9b25c2d91712ea7e5a286d6aae23dd841
Henry Ludemann <henry@munchkin>
2008-04-01 13:38:32 +0900

    Move decoder test class construction to a seperate module (so it can be used by other tests)

--- bdec/tools/test/testcompiler.py
+++ bdec/tools/test/testcompiler.py
@@ -1,12 +1,9 @@
 
-import glob
 import itertools
 import operator
 import os
 import os.path
-import shutil
 import StringIO
-import subprocess
 import unittest
 import xml.etree.ElementTree
 
@@ -17,7 +14,7 @@ import bdec.output.xmlout as xmlout
 import bdec.sequence as seq
 import bdec.sequenceof as sof
 import bdec.spec.expression as expr
-import bdec.tools.compiler as comp
+from bdec.test.decoders import create_decoder_classes
 
 import sys
 
@@ -331,60 +328,5 @@ class _CompilerTests:
         self._decode(b, 'b\00', common=[a,b])
         self._decode_failure(b, 'bac', common=[a,b])
 
-
-class _CompiledDecoder:
-    """Base class for testing decoders that are compiled."""
-    TEST_DIR = os.path.join(os.path.dirname(__file__), 'temp')
-    EXECUTABLE = os.path.join(TEST_DIR, 'decode')
-
-    def _compile(self, spec, common):
-        """Create a compiled decoder."""
-        if os.path.exists(self.TEST_DIR):
-            shutil.rmtree(self.TEST_DIR)
-        os.mkdir(self.TEST_DIR)
-
-        comp.generate_code(spec, self.TEMPLATE_PATH, self.TEST_DIR, common)
-
-        files = glob.glob(os.path.join(self.TEST_DIR, '*.%s' % self.FILE_TYPE))
-        if subprocess.call([self.COMPILER] + self.COMPILER_FLAGS + [self.EXECUTABLE] + files) != 0:
-            self.fail('Failed to compile!')
-
-    def _decode_file(self, spec, common, sourcefile):
-        """Return a tuple containing the exit code and the decoded xml."""
-        self._compile(spec, common)
-
-        filename = os.path.join(self.TEST_DIR, 'data.bin')
-        datafile = open(filename, 'wb')
-        datafile.write(sourcefile.read())
-        datafile.close()
-
-        decode = subprocess.Popen([self.EXECUTABLE, filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        xml = decode.stdout.read()
-        return decode.wait(), xml
-
-class _CDecoder(_CompiledDecoder):#(_CompilerTests, unittest.TestCase):
-    COMPILER = "gcc"
-    COMPILER_FLAGS = ["-Wall", '-g', '-o']
-    FILE_TYPE = "c"
-    TEMPLATE_PATH = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'templates', 'c')
-
-
-def create_decoder_classes(base_classes, name_prefix):
-    """
-    Return a dictionary of classes derived from unittest.TestCase.
-
-    There will be a derived base class for each type of decoder (eg: python, 
-    C, ...). The key of the dictionary is the name of the class, the value
-    the class itself.
-    
-    Can be used by globals().update(create_decoder_classes(...))
-    """
-    decoders = [(_CDecoder, 'C')]
-    result = {}
-    for base in base_classes:
-        for decoder, name in decoders:
-            test_name = "Test%s%s" % (name_prefix, name)
-            result[test_name] = type(test_name, (unittest.TestCase, decoder, base), {})
-    return result
 
 globals().update(create_decoder_classes([_CompilerTests], 'SimpleDecode'))
