b0a6f0f654f576e7b373f0e871ddf8a037c291b5
Henry Ludemann <misc@hl.id.au>
2010-04-10 18:56:20 +0800

    Fix the tests when using pyparsing.

--- bdec/parsing/test/testparsing.py
+++ bdec/parsing/test/testparsing.py
@@ -7,18 +7,18 @@ from bdec.parsing import *
 class TestParsing(unittest.TestCase):
     def test_word(self):
         a = Word('abcde')
-        self.assertEqual(['ebad'], a.parseString('ebad'))
+        self.assertEqual(['ebad'], list(a.parseString('ebad')))
 
     def test_zero_or_more(self):
         a = ZeroOrMore(Word('abcd'))
-        self.assertEqual(['ab', 'cd', 'dc'], a.parseString('  ab cd dc'))
+        self.assertEqual(['ab', 'cd', 'dc'], list(a.parseString('  ab cd dc')))
 
     def test_one_or_more(self):
         a = OneOrMore(Word('abcd'))
-        self.assertEqual(['ab', 'cd', 'dc'], a.parseString('  ab cd  dc  '))
+        self.assertEqual(['ab', 'cd', 'dc'], list(a.parseString('  ab cd  dc  ')))
         self.assertRaises(ParseException, a.parseString, 'ebcd')
 
     def test_string_end(self):
         a = Word('abcd') + StringEnd()
-        self.assertEqual(['aaaaa'], a.parseString(' aaaaa '))
+        self.assertEqual(['aaaaa'], list(a.parseString(' aaaaa ')))
         self.assertRaises(ParseException, a.parseString, ' aaaaa b')
