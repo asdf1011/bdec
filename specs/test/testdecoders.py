84cd09146be80b780f1009a866b0ec60a7590599
Henry Ludemann <henry@protocollogic.com>
2010-10-13 08:47:41 +0800

    Don't attempt to encode the specification files.
    In the future this code may use the same fixme as the regression, but not yet.

--- specs/test/testdecoders.py
+++ specs/test/testdecoders.py
@@ -29,7 +29,8 @@ class _BaseTest(object):
         else:
             datafile = open(filename, 'rb')
 
-        (exit_code, xml) = self._decode_file(self._spec, self._common, datafile)
+        (exit_code, xml) = self._decode_file(self._spec, self._common,
+                datafile, should_check_encoding=False)
         self.assertEqual(0, exit_code)
 
 
