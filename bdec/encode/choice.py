232a81a32181a22b40635a36e1c53b9c0eb30612
Henry Ludemann <henry@protocollogic.com>
2010-11-24 07:55:40 +0800

    Move the default option output parameter detection into global functions.
    This will allow it to be more easily used by the C template library.

--- bdec/encode/choice.py
+++ bdec/encode/choice.py
@@ -27,6 +27,92 @@ from bdec.sequence import Sequence
 from bdec.inspect.range import Range, Ranges
 from bdec.inspect.solver import solve_expression, SolverError
 
+def _get_unpopulated_outputs(choice, child):
+    """Find all outputs of other children that aren't outputs of child.
+
+    Returns a {name : params}."""
+    known = [p.name for p in child.params]
+    result = {}
+    for c in choice.children:
+        for p in c.params:
+            if p.direction == p.OUT and p.name not in known:
+                result.setdefault(p.name, []).append(p)
+    return result
+
+def _is_using_param(expr, param_name):
+    if isinstance(expr, ReferenceExpression):
+        return expr.name == param_name
+    elif isinstance(expr, ArithmeticExpression):
+        return _is_using_param(expr.left, param_name) or _is_using_param(expr.left, param_name)
+    return False
+
+def _solve(result_expr, sequence, param_name, expression_params):
+    constant, components = solve_expression(result_expr, sequence.value, sequence, expression_params, [])
+    if len(components) == 1:
+        reference, expression, inverted = components[0]
+        if reference.name == param_name:
+            return inverted.evaluate({})
+        else:
+            logging.warning("Expected to solve reference to '%s', but "
+                    "found '%s'! Skipping constraint." %
+                    (param_name, reference.name))
+    else:
+        logging.warning("Found multiple unknowns when solving '%s'! Skipping constraint." % sequence.value)
+
+def _get_ranges(entry, param_name, expression_params):
+    """ Get the ranges that param_name can be used for. """
+    if isinstance(entry, Sequence) and entry.value is not None and \
+            _is_using_param(entry.value, param_name):
+        # This entry is using the specified parameter; we want to find
+        # what values it expects it to be.
+        for constraint in entry.constraints:
+            value = _solve(constraint.limit, entry, param_name, expression_params)
+            if isinstance(constraint, Equals):
+                yield Range(value, value)
+            elif isinstance(constraint, Maximum):
+                yield Range(None, value)
+            elif isinstance(constraint, Minimum):
+                yield Range(value, None)
+
+    # Look in the children to see if a child entry uses the constraint...
+    for child in entry.children:
+        for local, param in zip(
+                expression_params.get_passed_variables(entry, child),
+                expression_params.get_params(child.entry)):
+            if local.name == param_name:
+                # We pass this parameter into the child. Recurse inwards...
+                for r in _get_ranges(child.entry, param.name, expression_params):
+                    yield r
+                break
+
+def get_default_option_params(choice, child, expression_params):
+    """Create a dictionary containing default values for output parameters
+    that aren't used by this option.
+
+    Returns a {param_name: value} dictionary."""
+    result = {}
+    outputs = _get_unpopulated_outputs(choice, child)
+    for name, params in outputs.items():
+        # Find the possible range of source values, and intersect it with
+        # the constraints placed on it by other earlier children.
+        possible = Ranges(p.type.range(expression_params) for p in params)
+        for c in choice.children[:choice.children.index(child)]:
+            for r in _get_ranges(c.encoder.entry, name, expression_params):
+                possible.remove(r)
+
+        # Use the possible value closest to zero.
+        positive = Range(0, None)
+        negative = Range(None, 0)
+        if possible.intersect(positive):
+            value = possible.intersect(positive)[0].min
+        elif possible.intersect(negative):
+            value = possible.intersect(positive)[-1].max
+        else:
+            value = 0
+            logging.warning('Unable to choose a good output for %s; using %s', name, value)
+        result[name] = value
+    return result
+
 class ChoiceEncoder(EntryEncoder):
     def _get_value(self, query, parent, offset, name, context):
         try:
@@ -35,88 +121,6 @@ class ChoiceEncoder(EntryEncoder):
             # Choice entries can be completely hidden
             return parent
 
-    def _get_unpopulated_outputs(self, child):
-        """Find all outputs of other children that aren't outputs of child.
-
-        Returns a {name : params}."""
-        known = [p.name for p in child.params]
-        result = {}
-        for c in self.children:
-            for p in c.params:
-                if p.direction == p.OUT and p.name not in known:
-                    result.setdefault(p.name, []).append(p)
-        return result
-
-    def _is_using_param(self, expr, param_name):
-        if isinstance(expr, ReferenceExpression):
-            return expr.name == param_name
-        elif isinstance(expr, ArithmeticExpression):
-            return self._is_using_param(expr.left, param_name) or self._is_using_param(expr.left, param_name)
-        return False
-
-    def _solve(self, result_expr, sequence, param_name):
-        constant, components = solve_expression(result_expr, sequence.value, sequence, self._params, [])
-        if len(components) == 1:
-            reference, expression, inverted = components[0]
-            if reference.name == param_name:
-                return inverted.evaluate({})
-            else:
-                logging.warning("Expected to solve reference to '%s', but "
-                        "found '%s'! Skipping constraint." %
-                        (param_name, reference.name))
-        else:
-            logging.warning("Found multiple unknowns when solving '%s'! Skipping constraint." % sequence.value)
-
-    def _get_ranges(self, entry, param_name):
-        """ Get the ranges that param_name can be used for. """
-        if isinstance(entry, Sequence) and entry.value is not None and \
-                self._is_using_param(entry.value, param_name):
-            # This entry is using the specified parameter; we want to find
-            # what values it expects it to be.
-            for constraint in entry.constraints:
-                value = self._solve(constraint.limit, entry, param_name)
-                if isinstance(constraint, Equals):
-                    yield Range(value, value)
-                elif isinstance(constraint, Maximum):
-                    yield Range(None, value)
-                elif isinstance(constraint, Minimum):
-                    yield Range(value, None)
-
-        # Look in the children to see if a child entry uses the constraint...
-        for child in entry.children:
-            for local, param in zip(
-                    self._params.get_passed_variables(entry, child),
-                    self._params.get_params(child.entry)):
-                if local.name == param_name:
-                    # We pass this parameter into the child. Recurse inwards...
-                    for r in self._get_ranges(child.entry, param.name):
-                        yield r
-                    break
-
-    def _create_mock_out_params(self, child):
-        """Create a context variable containing any mocked output parameters."""
-        result = {}
-        outputs = self._get_unpopulated_outputs(child)
-        for name, params in outputs.items():
-            # Find the possible range of source values, and intersect it with
-            # the constraints placed on it by other earlier children.
-            possible = Ranges(p.type.range(self._params) for p in params)
-            for c in self.children[:self.children.index(child)]:
-                for r in self._get_ranges(c.encoder.entry, name):
-                    possible.remove(r)
-
-            # Use the possible value closest to zero.
-            positive = Range(0, None)
-            negative = Range(None, 0)
-            if possible.intersect(positive):
-                value = possible.intersect(positive)[0].min
-            elif possible.intersect(negative):
-                value = possible.intersect(positive)[-1].max
-            else:
-                value = 0
-                logging.warning('Unable to choose a good output for %s; using %s', name, value)
-            result[name] = value
-        return result
 
     def _encode(self, query, value, context):
         # We attempt to encode all of the embedded items, until we find
@@ -143,5 +147,5 @@ class ChoiceEncoder(EntryEncoder):
                     best_guess_bits = bits_encoded
 
         result = self._encode_child(best_guess, query, value, 0, context)
-        context.update(self._create_mock_out_params(best_guess))
+        context.update(get_default_option_params(self, best_guess, self._params))
         return result
