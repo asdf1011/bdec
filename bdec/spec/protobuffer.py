da015df98efae285647106e5f75756d05b88d669
Henry Ludemann <henry@protocollogic.com>
2013-02-08 19:29:31 +0800

    Integrate the protocol buffer decoder into the specification infrasturcture.
    Also move the inline xml to its own xml file.

--- bdec/spec/protobuffer.py
+++ bdec/spec/protobuffer.py
@@ -4,6 +4,7 @@ from bdec.entry import Child
 from bdec.expression import parse
 from bdec.sequence import Sequence
 import bdec.spec.xmlspec
+import os.path
 from pyparsing import alphanums, Literal, nums, OneOrMore, ParseException, StringEnd, Word
 
 class _Locator:
@@ -25,83 +26,58 @@ class Error(bdec.spec.LoadErrorWithLocation):
     def __str__(self):
         return self._src() + self._message
 
-_var_int = None
-def _createVarInt():
-    """ Create an decoder for variable integers. """
-    global _var_int
-    if not _var_int:
-        spec = '''
-          <protocol>
-            <reference name="varint" />
-            <common>
-              <sequence name="varint" value="${embedded:}" >
-                <choice name="embedded:">
-                  <sequence name="single byte" value="${value}">
-                    <field length="1" value="0" />
-                    <field name="value" length="7" type="integer" />
-                  </sequence>
-                  <sequence name="multi byte" value="${varint} * 128 + ${value}" >
-                    <field length="1" value="1" />
-                    <field name="value" length="7" type="integer" />
-                    <reference name="varint" />
-                  </sequence>
-                </choice>
-              </sequence>
-            </common>
-          </protocol>
-          '''
-        _var_int = bdec.spec.load_specs([('varint.xml', spec, None)])[0]
-    return _var_int
+class _Parser:
+    def __init__(self, references):
+        rule = Literal('required') | 'optional' | 'repeated'
+        type = rule + Word(alphanums) + Word(alphanums) + '=' + Word(nums) + ';'
+        message = 'message' + Word(alphanums) + '{' + OneOrMore(type) + '}'
+        self._parser = OneOrMore(message) + StringEnd()
 
-def _create_int32(name):
-    return 0, Sequence(name, [Child('value:', _createVarInt())], value=parse("${value:}"))
+        type.addParseAction(lambda s,l,t: self._createType(t[0], t[1], t[2], int(t[4])))
+        message.addParseAction(lambda s,l,t:self._createMessage(t[1], t[3:-1]))
 
-# A lookup of type name to handling function. The handing function takes
-# the name and returns a (entry, typeNumber) tuple.
-_type_lookup = {'int32' : _create_int32}
+        # A lookup of type name to handling function. The handing function takes
+        # the name and returns a (entry, typeNumber) tuple.
+        self._type_lookup = {'int32' : self._create_int32}
+        self._references = references
 
-def _createType(rule, type, name, fieldNumber):
-    if rule != 'required':
-        raise Exception('Only handlle required at the moment!')
-    wire_type, entry = _type_lookup[type](name)
+    def _create_int32(self, name):
+        return 0, Child(name,self._references.get_common('int32'))
 
-    keyValue = fieldNumber << 3 | wire_type
-    key = Sequence('key:', [_createVarInt()], value=parse("${varint}"), constraints=[Equals(keyValue)])
-    return [key, entry]
+    def _createType(self, rule, type, name, fieldNumber):
+        if rule != 'required':
+            raise NotImplementedError('Only handlle required at the moment!')
+        wire_type, entry = self._type_lookup[type](name)
 
-def _createMessage(name, types):
-    return Sequence(name, types)
+        keyValue = fieldNumber << 3 | wire_type
+        key = Sequence('key:', [self._references.get_common('varint')],
+                value=parse("${varint}"), constraints=[Equals(keyValue)])
+        return [key, entry]
 
-def loads(text, filename=None):
-    """Load a protocol buffer spec from a string.
+    def _createMessage(self, name, types):
+        return Sequence(name, types)
 
-    text -- The protocol buffer specification in a string.
+    def parse(self, text):
+        return list(self._parser.parseString(text))
+
+def load(filename, contents, references):
+    """Load a protocol buffer spec.
+
     filename -- The filename of the specification.
-    return -- A tuple of (messages, common, lookup), where messages is a list of
-        bdec.entry.Entry instances.
+    contents -- The protocol buffer specification in a string.
+    references -- A bdec.spec.refernce.References instance.
+    return -- A tuple of (decoder, lookup).
     """
-    rule = Literal('required') | 'optional' | 'repeated'
-    type = rule + Word(alphanums) + Word(alphanums) + '=' + Word(nums) + ';'
-    message = 'message' + Word(alphanums) + '{' + OneOrMore(type) + '}'
-    complete = OneOrMore(message) + StringEnd()
+    # Load the parts of protocol buffers implemented in bdec
+    proto_filename = os.path.join(os.path.dirname(__file__), '..', '..', 'specs', 'protobuffer.xml')
+    generic_spec, lookup = bdec.spec.xmlspec.load(proto_filename, file(proto_filename, 'r'), references)
 
-    type.addParseAction(lambda s,l,t: _createType(t[0], t[1], t[2], int(t[4])))
-    message.addParseAction(lambda s,l,t: _createMessage(t[1], t[3:-1]))
-
+    parser = _Parser(references)
     try:
-        entries = list(complete.parseString(text))
+        entries = parser.parse(contents.read())
     except ParseException, ex:
         raise Error(filename, ex.lineno, ex.col, ex)
-    return entries, entries + [_createVarInt()], {}
-
-def load(file):
-    """Load a protocol buffer spec from a file.
-
-    file -- Either a filename, or a file type object.
-    """
-    filename = None
-    if isinstance(file, basestring):
-        filename = file
-        file = load(filename, 'rb')
-    return loads(file.read(), filename)
+    for entry in entries:
+        references.add_common(entry)
+    return entries[0], lookup
 
--- bdec/spec/test/testprotobuffer.py
+++ bdec/spec/test/testprotobuffer.py
@@ -1,6 +1,6 @@
 
 from bdec.data import Data
-from bdec.spec.protobuffer import loads
+from bdec.spec import load_specs
 from bdec.output.instance import decode
 import unittest
 
@@ -20,8 +20,7 @@ class TestProtoBuffer(unittest.TestCase):
           message Integer {
             required int32 value = 1;
           } '''
-        messages, common, lookup = loads(text)
-        self.assertEqual(1, len(messages))
-        data = decode(messages[0], Data('\x08\x96\x01'))
+        entry, common, lookup = load_specs([('test.proto', text, None)])
+        data = decode(entry, Data('\x08\x96\x01'))
         self.assertEqual(150, data.value)
 
