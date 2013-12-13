511cded4b7f2546329178ca8d24b1b3f65386b2c
Henry Ludemann <henry@protocollogic.com>
2013-12-13 18:56:25 +0800

    Update the changelog and version to 0.8.0.

--- bdec/__init__.py
+++ bdec/__init__.py
@@ -36,7 +36,7 @@ It can:
  * Be defined in a textual format (bdec.spec)
 """
 
-__version__ = "0.7.2"
+__version__ = "0.8.0"
 
 class DecodeError(Exception):
     """ An error raise when decoding fails """
